915a885d02106d24b555e1e0120cb46e9126fb81
sirliu <sirliu110@Gmail.com>
2023-12-04 10:50:15 +0800

    增加读取excel指定列序或者指定标题的列数据

--- utils.go
+++ utils.go
@@ -125,3 +125,64 @@ func WriteToExcel(df dataframe.DataFrame, outputPath string) error {
 	}
 	return nil
 }
+
+// ReadExcelCol 根据文件路径、Sheet名称、列索引或标题读取指定列的数据
+// 如果 sheetName 为空，则默认使用第一个 Sheet
+// colIndex 是列的数字索引（从1开始），title 是列的标题
+// 两者只能传一个，如果两者都传或都不传，将返回错误
+func ReadExcelCol(filePath, title string, colIndex int, sheetName string) ([]string, error) {
+	// 检查参数
+	if (colIndex != 0 && title != "") || (colIndex == 0 && title == "") {
+		return nil, fmt.Errorf("必须且只能指定 colIndex 或 title 其中一个")
+	}
+
+	f, err := excelize.OpenFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// 如果没有指定 sheetName，则使用第一个 Sheet
+	if sheetName == "" {
+		sheets := f.GetSheetList()
+		if len(sheets) == 0 {
+			return nil, fmt.Errorf("Excel File Specifal had no Sheet")
+		}
+		sheetName = sheets[0]
+	}
+
+	var colNum int
+	if colIndex != 0 {
+		colNum = colIndex
+	} else {
+		colNum, err = FindColumnIndexByTitle(f, sheetName, title)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	cols, err := f.GetCols(sheetName)
+	if err != nil {
+		return nil, err
+	}
+	if colNum <= 0 || colNum > len(cols) {
+		return nil, fmt.Errorf("找不到指定的列")
+	}
+
+	return cols[colNum-1], nil
+}
+
+// 按照列名查找列
+func FindColumnIndexByTitle(f *excelize.File, sheetName, title string) (int, error) {
+	rows, err := f.GetRows(sheetName)
+	if err != nil {
+		return 0, err
+	}
+
+	for c, cell := range rows[0] { // 假设标题在第一行
+		if cell == title {
+			return c + 1, nil
+		}
+	}
+	return 0, fmt.Errorf("找不到标题为 %s 的列", title)
+}
